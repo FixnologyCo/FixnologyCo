5808ae669663de7798e8759306b231f18db63957
JhoannS <sebastianzamudio2004@gmail.com>
2025-08-11 15:49:17 -0500

    feat: Integrar componente InputTexto en ModalCrearUsuario y mejorar la lógica de validación

--- resources/js/Components/Modales/FixnologyCO/Usuarios/ModalCrearUsuario.vue
+++ resources/js/Components/Modales/FixnologyCO/Usuarios/ModalCrearUsuario.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps, defineEmits, ref, computed } from "vue";
 import { useForm } from "@inertiajs/vue3";
+import InputTexto from "@/Components/Shared/inputs/InputTexto.vue";
+import { handleBlur, handleInput, limitesCaracteres } from "@/Utils/formateoInputs";
 
 const props = defineProps({
   isOpen: {
@@ -55,14 +57,12 @@ function nextStep() {
     currentStep.value++;
   }
 }
-
 function prevStep() {
   if (currentStep.value > 1) {
     currentStep.value--;
   }
 }
 
-// --- LÓGICA DE ENVÍO ---
 const submit = () => {
   form.post(route("usuarios.store"), {
     preserveScroll: true,
@@ -73,53 +73,49 @@ const submit = () => {
     },
   });
 };
-import InputTexto from "@/Components/Shared/inputs/InputTexto.vue";
-import InputTexto from "@/Components/Shared/inputs/InputTexto.vue";
-function handleBlur(formObject, fieldName) {
-  // Tu lógica de validación al perder el foco
-  if (!formObject[fieldName]) {
-    formObject.setError(fieldName, 'Este campo es requerido.');
-  } else {
-    formObject.clearErrors(fieldName);
-  }
-}
 </script>
 
 <template>
   <Transition name="modal-fade" appear>
-    <div v-if="isOpen"
+    <div
+      v-if="isOpen"
       class="fixed inset-0 z-50 flex items-center justify-center bg-mono-negro_opacity_full backdrop-blur-sm"
-     >
-      <button type="button" @click="$emit('close')" class="absolute top-5 right-5 text-gray-400 hover:text-white focus:outline-none">
-              <span class="material-symbols-rounded text-[30px]">close</span>
-            </button>
+    >
+      <button
+        type="button"
+        @click="$emit('close')"
+        class="absolute top-5 right-5 text-gray-400 hover:text-white focus:outline-none"
+      >
+        <span class="material-symbols-rounded text-[30px]">close</span>
+      </button>
       <Transition name="modal-slide" appear>
-        <form @submit.prevent="submit"
-          class="relative bg-mono-negro rounded-xl shadow-lg w-full max-w-[65%] border border-secundary-light text-gray-300 flex flex-col p-5">
+        <form
+          @submit.prevent="submit"
+          class="relative bg-mono-negro rounded-xl shadow-lg w-full max-w-[65%] border border-secundary-light text-gray-300 flex flex-col p-5"
+        >
           <!-- Barra de Progreso -->
           <div class="flex gap-1 items-center">
             <div class="w-full flex justify-between bg-gray-600 rounded-full h-1">
               <div
                 class="bg-primary shadow-[0px_5px_24px] shadow-primary h-1 rounded-full transition-all duration-500 ease-out"
-                :style="{ width: progressPercentage + '%' }"></div>
-
-
+                :style="{ width: progressPercentage + '%' }"
+              ></div>
             </div>
             <p class="text-[12px]">{{ Math.round(progressPercentage) }}%</p>
           </div>
 
-
           <!-- Encabezado -->
           <div class="py-4 flex justify-between items-center flex-shrink-0">
             <h3 class="text-xl font-semibold text-gray-100 flex items-center">
-              <span class="material-symbols-rounded align-middle mr-2 text-primary">person_add</span>
+              <span class="material-symbols-rounded align-middle mr-2 text-primary"
+                >person_add</span
+              >
               Crear Nuevo Usuario
             </h3>
-            
           </div>
 
           <!-- Cuerpo del Formulario con Transición -->
-          <div class=" overflow-hidden">
+          <div class="overflow-hidden">
             <Transition name="slide-fade" mode="out-in">
               <div :key="currentStep" class="space-y-4">
                 <!-- PASO 1: DATOS PERSONALES -->
@@ -128,41 +124,72 @@ function handleBlur(formObject, fieldName) {
                     Datos de la Cuenta
                   </h4>
                   <div class="space-y-4">
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                      <div>
-                        <label for="primer_nombre" class="block text-sm font-medium text-gray-400">Primer Nombre</label>
-                        <input v-model="form.primer_nombre" type="text" id="primer_nombre" class="input-field" />
-                        <div v-if="form.errors.primer_nombre" class="error-message">
-                          {{ form.errors.primer_nombre }}
-                        </div>
-                      </div>
-                      <div>
-                        <label for="primer_apellido" class="block text-sm font-medium text-gray-400">Primer
-                          Apellido</label>
-                        <input v-model="form.primer_apellido" type="text" id="primer_apellido" class="input-field" />
-                        <div v-if="form.errors.primer_apellido" class="error-message">
-                          {{ form.errors.primer_apellido }}
-                        </div>
-                      </div>
+                    <div
+                      class="2xl:flex 2xl:flex-row 2xl:justify-between 2xl:items-center 2xl:gap-2 xl:flex xl:flex-row xl:justify-between xl:items-center xl:gap-2 gap-3 flex flex-col items-center"
+                    >
+                      <InputTexto
+                        v-model="form.primer_nombre"
+                        label="Primer nombre:"
+                        icon="format_italic"
+                        placeholder="Ej: Juan"
+                        :maxLength="limitesCaracteres.primer_nombre"
+                        :error="form.errors.primer_nombre"
+                        @blur="handleBlur(form, 'primer_nombre')"
+                        @input="(e) => handleInput(e, form, 'primer_nombre')"
+                      />
+
+                      <InputTexto
+                        v-model="form.primer_apellido"
+                        label="Primer apellido:"
+                        icon="format_italic"
+                        placeholder="Ej: Guarnizo"
+                        :maxLength="limitesCaracteres.primer_apellido"
+                        :error="form.errors.primer_apellido"
+                        @blur="handleBlur(form, 'primer_apellido')"
+                      />
                     </div>
                     <div>
-                      <label for="correo" class="block text-sm font-medium text-gray-400">Correo Electrónico</label>
-                      <input v-model="form.correo" type="email" id="correo" class="input-field" />
+                      <label for="correo" class="block text-sm font-medium text-gray-400"
+                        >Correo Electrónico</label
+                      >
+                      <input
+                        v-model="form.correo"
+                        type="email"
+                        id="correo"
+                        class="input-field"
+                      />
                       <div v-if="form.errors.correo" class="error-message">
                         {{ form.errors.correo }}
                       </div>
                     </div>
                     <div>
-                      <label for="numero_documento" class="block text-sm font-medium text-gray-400">Número de
-                        Documento</label>
-                      <input v-model="form.numero_documento" type="text" id="numero_documento" class="input-field" />
+                      <label
+                        for="numero_documento"
+                        class="block text-sm font-medium text-gray-400"
+                        >Número de Documento</label
+                      >
+                      <input
+                        v-model="form.numero_documento"
+                        type="text"
+                        id="numero_documento"
+                        class="input-field"
+                      />
                       <div v-if="form.errors.numero_documento" class="error-message">
                         {{ form.errors.numero_documento }}
                       </div>
                     </div>
                     <div>
-                      <label for="password" class="block text-sm font-medium text-gray-400">Contraseña</label>
-                      <input v-model="form.password" type="password" id="password" class="input-field" />
+                      <label
+                        for="password"
+                        class="block text-sm font-medium text-gray-400"
+                        >Contraseña</label
+                      >
+                      <input
+                        v-model="form.password"
+                        type="password"
+                        id="password"
+                        class="input-field"
+                      />
                       <div v-if="form.errors.password" class="error-message">
                         {{ form.errors.password }}
                       </div>
@@ -174,12 +201,20 @@ function handleBlur(formObject, fieldName) {
                 <div v-if="currentStep === 2">
                   <h4 class="font-semibold text-lg mb-4 text-primary">Tipo de Usuario</h4>
                   <div class="space-y-3">
-                    <label class="flex items-center p-3 rounded-md transition-colors" :class="form.tipo_usuario === 'propietario'
-                        ? 'bg-primary/10 border border-primary'
-                        : 'bg-gray-800/50 border border-transparent hover:border-gray-700'
-                      ">
-                      <input type="radio" v-model="form.tipo_usuario" value="propietario"
-                        class="form-radio text-primary focus:ring-primary bg-gray-700 border-gray-600" />
+                    <label
+                      class="flex items-center p-3 rounded-md transition-colors"
+                      :class="
+                        form.tipo_usuario === 'propietario'
+                          ? 'bg-primary/10 border border-primary'
+                          : 'bg-gray-800/50 border border-transparent hover:border-gray-700'
+                      "
+                    >
+                      <input
+                        type="radio"
+                        v-model="form.tipo_usuario"
+                        value="propietario"
+                        class="form-radio text-primary focus:ring-primary bg-gray-700 border-gray-600"
+                      />
                       <span class="ml-3">
                         <span class="font-semibold">Nuevo Propietario</span>
                         <p class="text-xs text-gray-400">
@@ -187,12 +222,20 @@ function handleBlur(formObject, fieldName) {
                         </p>
                       </span>
                     </label>
-                    <label class="flex items-center p-3 rounded-md transition-colors" :class="form.tipo_usuario === 'empleado'
-                        ? 'bg-primary/10 border border-primary'
-                        : 'bg-gray-800/50 border border-transparent hover:border-gray-700'
-                      ">
-                      <input type="radio" v-model="form.tipo_usuario" value="empleado"
-                        class="form-radio text-primary focus:ring-primary bg-gray-700 border-gray-600" />
+                    <label
+                      class="flex items-center p-3 rounded-md transition-colors"
+                      :class="
+                        form.tipo_usuario === 'empleado'
+                          ? 'bg-primary/10 border border-primary'
+                          : 'bg-gray-800/50 border border-transparent hover:border-gray-700'
+                      "
+                    >
+                      <input
+                        type="radio"
+                        v-model="form.tipo_usuario"
+                        value="empleado"
+                        class="form-radio text-primary focus:ring-primary bg-gray-700 border-gray-600"
+                      />
                       <span class="ml-3">
                         <span class="font-semibold">Empleado de Tienda Existente</span>
                         <p class="text-xs text-gray-400">
@@ -200,13 +243,27 @@ function handleBlur(formObject, fieldName) {
                         </p>
                       </span>
                     </label>
-                    <div v-if="form.tipo_usuario === 'empleado'" class="space-y-4 pt-4 border-t border-gray-700/50">
+                    <div
+                      v-if="form.tipo_usuario === 'empleado'"
+                      class="space-y-4 pt-4 border-t border-gray-700/50"
+                    >
                       <div>
-                        <label for="establecimiento_id" class="block text-sm font-medium text-gray-400">Asignar a
-                          Establecimiento</label>
-                        <select v-model="form.establecimiento_id" id="establecimiento_id" class="input-field">
+                        <label
+                          for="establecimiento_id"
+                          class="block text-sm font-medium text-gray-400"
+                          >Asignar a Establecimiento</label
+                        >
+                        <select
+                          v-model="form.establecimiento_id"
+                          id="establecimiento_id"
+                          class="input-field"
+                        >
                           <option :value="null" disabled>Selecciona una tienda</option>
-                          <option v-for="est in establecimientosDisponibles" :key="est.id" :value="est.id">
+                          <option
+                            v-for="est in establecimientosDisponibles"
+                            :key="est.id"
+                            :value="est.id"
+                          >
                             {{ est.nombre_establecimiento }}
                           </option>
                         </select>
@@ -215,8 +272,15 @@ function handleBlur(formObject, fieldName) {
                         </div>
                       </div>
                       <div>
-                        <label for="cargo" class="block text-sm font-medium text-gray-400">Cargo del Empleado</label>
-                        <input v-model="form.cargo" type="text" id="cargo" class="input-field" />
+                        <label for="cargo" class="block text-sm font-medium text-gray-400"
+                          >Cargo del Empleado</label
+                        >
+                        <input
+                          v-model="form.cargo"
+                          type="text"
+                          id="cargo"
+                          class="input-field"
+                        />
                         <div v-if="form.errors.cargo" class="error-message">
                           {{ form.errors.cargo }}
                         </div>
@@ -225,50 +289,63 @@ function handleBlur(formObject, fieldName) {
                   </div>
                 </div>
                 <div v-if="currentStep === 3">
-                
-                <div class="2xl:flex 2xl:flex-row 2xl:justify-between 2xl:items-center 2xl:gap-2 xl:flex xl:flex-row xl:justify-between xl:items-center xl:gap-2 gap-3 flex flex-col items-center">
-    
-    <!-- 2. Reemplaza el bloque del primer nombre -->
-    <InputTexto
-      v-model="form.primer_nombre"
-      label="Primer nombre:"
-      icon="format_italic"
-      placeholder="Ej: Juan"
-      :maxLength="limitesCaracteres.primer_nombre"
-      :error="form.errors.primer_nombre"
-      @blur="handleBlur(form, 'primer_nombre')"
-    />
+                  <div
+                    class="2xl:flex 2xl:flex-row 2xl:justify-between 2xl:items-center 2xl:gap-2 xl:flex xl:flex-row xl:justify-between xl:items-center xl:gap-2 gap-3 flex flex-col items-center"
+                  >
+                    <!-- 2. Reemplaza el bloque del primer nombre -->
+                    <InputTexto
+                      v-model="form.primer_nombre"
+                      label="Primer nombre:"
+                      icon="format_italic"
+                      placeholder="Ej: Juan"
+                      :maxLength="limitesCaracteres.primer_nombre"
+                      :error="form.errors.primer_nombre"
+                      @blur="handleBlur(form, 'primer_nombre')"
+                    />
 
-    <!-- 3. Reemplaza el bloque del primer apellido -->
-    <InputTexto
-      v-model="form.primer_apellido"
-      label="Primer apellido:"
-      icon="format_italic"
-      placeholder="Ej: Guarnizo"
-      :maxLength="limitesCaracteres.primer_apellido"
-      :error="form.errors.primer_apellido"
-      @blur="handleBlur(form, 'primer_apellido')"
-    />
-
-  </div>
+                    <!-- 3. Reemplaza el bloque del primer apellido -->
+                    <InputTexto
+                      v-model="form.primer_apellido"
+                      label="Primer apellido:"
+                      icon="format_italic"
+                      placeholder="Ej: Guarnizo"
+                      :maxLength="limitesCaracteres.primer_apellido"
+                      :error="form.errors.primer_apellido"
+                      @blur="handleBlur(form, 'primer_apellido')"
+                    />
+                  </div>
                 </div>
               </div>
             </Transition>
           </div>
 
           <!-- Pie del Modal -->
-          <div class="px-6 py-4 bg-gray-800/50 flex justify-between items-center flex-shrink-0">
-            <button v-if="currentStep > 1" type="button" @click="prevStep"
-              class="text-gray-300 rounded-md py-2 px-4 hover:bg-gray-700 transition-colors">
+          <div
+            class="px-6 py-4 bg-gray-800/50 flex justify-between items-center flex-shrink-0"
+          >
+            <button
+              v-if="currentStep > 1"
+              type="button"
+              @click="prevStep"
+              class="text-gray-300 rounded-md py-2 px-4 hover:bg-gray-700 transition-colors"
+            >
               Atrás
             </button>
             <div v-else></div>
-            <button v-if="currentStep < totalSteps" type="button" @click="nextStep"
-              class="bg-primary text-white rounded-md shadow-sm py-2 px-4 hover:bg-opacity-90 transition-colors">
+            <button
+              v-if="currentStep < totalSteps"
+              type="button"
+              @click="nextStep"
+              class="bg-primary text-white rounded-md shadow-sm py-2 px-4 hover:bg-opacity-90 transition-colors"
+            >
               Siguiente
             </button>
-            <button v-else type="submit" :disabled="form.processing"
-              class="bg-green-600 text-white rounded-md shadow-sm py-2 px-4 hover:bg-green-500 disabled:opacity-50 transition-colors">
+            <button
+              v-else
+              type="submit"
+              :disabled="form.processing"
+              class="bg-green-600 text-white rounded-md shadow-sm py-2 px-4 hover:bg-green-500 disabled:opacity-50 transition-colors"
+            >
               Crear Usuario
             </button>
           </div>
